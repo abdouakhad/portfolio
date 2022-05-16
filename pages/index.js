9362589fd56c92a9dd3cbc392c428096a61cc50c
Abdoul Akhad Top <akhad0015@gmail.com>
2022-05-16 20:08:37 +0000

    complete fixed name error

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import HomePage from '../components/home'
-import AboutPage from '../components/about'
-import ResumePage from '../components/resume'
-import Services from '../components/services'
-import Contact from '../components/contact'
+import React from "react"
+import HomePage from "../components/Home"
+import AboutPage from "../components/About"
+import ResumePage from "../components/Resume"
+import Services from "../components/Services"
+import Contact from "../components/Contact"
 const Home = () => {
   return (
-    <div className=''>
+    <div className="">
       <HomePage />
       <AboutPage />
       <ResumePage />
